7a6aa7fae3c1fb5674a835defd1c49bf4c79da26
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-03-11 13:51:10 +0530

    added auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );

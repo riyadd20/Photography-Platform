b4e6128620ee1bc83f3040739edb551b4c9ae8af
AnushkaM02 <anushka.mulik@gmail.com>
2023-03-11 16:56:41 +0530

    installed required packages

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import store from './store/store';
+import store from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

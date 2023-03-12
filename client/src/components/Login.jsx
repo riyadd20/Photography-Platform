99afdf35feeb28b3b5ed271d9c8903b6859e20e6
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-03-12 06:11:28 +0530

    minor changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   useEffect(() => {
     if (user?._id) {
-      navigate("/home");
+      navigate("/");
     }
   }, [user]);
 
@@ -30,7 +30,7 @@ const Login = () => {
       );
       console.log(response.data);
       dispatch(setLogin(response.data));
-      navigate("/home");
+      navigate("/");
     } catch (error) {
       console.log(error.message);
     }
